16bb4a8aff1d1db054caa89b9513dcf119d44d6e
Shivam gupta <sguupta1214@gmail.com>
2025-01-15 05:42:24 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,48 @@
-import express from "express"
-import connectDB from './lib/connectDB.js'
-import userRouter from "./routes/user.route.js"
-import postRouter from "./routes/post.route.js"
-import commentRouter from './routes/comment.route.js'
-import webhookRouter from './routes/webhook.route.js'
-import cors from 'cors'
-import {clerkMiddleware} from '@clerk/express'
-
-
+import express from "express";
+import connectDB from './lib/connectDB.js';
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from './routes/comment.route.js';
+import webhookRouter from './routes/webhook.route.js';
+import cors from 'cors';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 
-app.use(cors(process.env.CLIENT_URL));
-app.use(clerkMiddleware())
+const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:5173"];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
 
-app.use("/webhooks",webhookRouter)
+app.use(clerkMiddleware());
 app.use(express.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
+
+app.use("/webhooks", webhookRouter);
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        message: error.message || "Something went wrong",
+        status: error.status,
+        stack: error.stack
+    });
 });
 
-
-app.use("/users",userRouter);
-app.use("/posts",postRouter);
-app.use("/comments",commentRouter);
-
-
-app.use((error,req,res,next)=>{
-    res.status(
-        error.status || 500
-    )
-    res.json({
-        message:error.message || "Something went wrong",
-        status:error.status,
-        stack:error.stack
-    })
-})
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     connectDB();
     console.log("Server is running");
-})
+});
